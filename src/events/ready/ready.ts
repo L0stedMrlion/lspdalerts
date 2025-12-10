0a253b39b5f0eb490afe5571c906f3b52afbf23a
Mrlion <mrlion4202@gmail.com>
2025-12-10 21:34:19 +0100

    Update ready.ts

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -4,19 +4,8 @@ import { CommandKit } from "commandkit";
 export default (c: Client<true>, client: Client<true>, handler: CommandKit) => {
   console.log(`✅ ${c.user.tag} is online.`);
 
-  const statuses = [
-    { name: `🚨 All alerts`, type: ActivityType.Watching },
-    { name: `🥷 Metro alerts`, type: ActivityType.Watching },
-    { name: `🕵️ Detective alerts`, type: ActivityType.Watching },
-  ];
-
-  let currentIndex = 0;
-
-  client.user.setActivity(statuses[currentIndex]);
-
-  setInterval(() => {
-    currentIndex = (currentIndex + 1) % statuses.length;
-    client.user.setActivity(statuses[currentIndex]);
-  }, 600000);
+  client.user.setActivity({
+    name: `🚨 All alerts`,
+    type: ActivityType.Listening,
+  });
 };
-
