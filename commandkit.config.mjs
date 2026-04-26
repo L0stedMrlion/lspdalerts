87e97df9e777fc79f7094017b5e3e6d998bb1287
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-04-26 07:09:18 +0000

    test

--- commandkit.config.mjs
+++ commandkit.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "commandkit";
 
 export default defineConfig({
-  commands: "src/app/commands",
-  eventsPath: path.join(__dirname, 'events'),
   plugins: [],
   devGuildIds: [],
   devUserIds: ["710549603216261141"],
