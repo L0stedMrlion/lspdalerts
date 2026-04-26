bc62b7048de25552e4d54f3a1389df55703f3da8
Mrlion <87368344+L0stedMrlion@users.noreply.github.com>
2026-04-26 07:06:51 +0000

    test

--- .commandkit/index.js
+++ .commandkit/index.js
@@ -1,15 +1,14 @@
 import "dotenv/config";
 import { Client, IntentsBitField } from "discord.js";
-import { commandkit } from "commandkit";
+import { CommandKit } from "commandkit";
 
 //#region src/app/index.ts
-const client = new Client({ intents: [
+const commandkit = new CommandKit({ client: new Client({ intents: [
 	IntentsBitField.Flags.Guilds,
 	IntentsBitField.Flags.GuildMembers,
 	IntentsBitField.Flags.GuildMessages,
 	IntentsBitField.Flags.MessageContent
-] });
-commandkit.setClient(client);
+] }) });
 if (process.env.COMMANDKIT_IS_CLI === "true") commandkit.start();
 
 //#endregion
--- commandkit.config.mjs
+++ commandkit.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "commandkit";
 
 export default defineConfig({
   commands: "src/app/commands",
-  events: "src/app/events",
+  eventsPath: path.join(__dirname, 'events'),
   plugins: [],
   devGuildIds: [],
   devUserIds: ["710549603216261141"],
